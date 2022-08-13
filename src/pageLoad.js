a3f8db6b214d1f2040f3838e043d7a0626efffbc
ayrton <ayrton.parkinson1@gmail.com>
2022-08-13 16:17:55 -0400

    Import and add mainSection and footerElement to content div on pageLoad()

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,8 +1,12 @@
 import navElement from "./navbar";
+import mainDiv from "./mainSection";
+import footerElement from "./footer";
 
-const pageLoad = window.addEventListener('DOMContentLoaded', () => {
+const pageLoad = () => {
   const contentDiv = document.getElementById('content');
   contentDiv.appendChild(navElement);
-})
+  contentDiv.appendChild(mainDiv)
+  contentDiv.appendChild(footerElement);
+};
 
 export default pageLoad;
