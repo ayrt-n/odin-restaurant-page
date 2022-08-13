2a49d01e64939f993d3f57487a092a5db23bb18d
ayrton <ayrton.parkinson1@gmail.com>
2022-08-13 16:17:11 -0400

    Create mainSection module to dynamically generate main section HTML

---
+++ src/mainSection.js
@@ -0,0 +1,18 @@
+// Create main section div
+const mainDiv = document.createElement('div');
+mainDiv.classList.add('main-section');
+
+// Create content for main section
+const hero = document.createElement('div');
+hero.classList.add('hero');
+hero.innerHTML = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using "Content here, content here", making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for "lorem ipsum" will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).';
+
+// Append content to main section and export
+mainDiv.appendChild(hero);
+
+export default mainDiv; 
+
+// Main section structure
+/* <div class="main-section">
+  <div class="hero">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</div>
+</div> */
