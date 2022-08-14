1c0d31114626df7915f019da2c40c3ffe00cdab9
ayrton <ayrton.parkinson1@gmail.com>
2022-08-14 09:27:44 -0400

    Add module rules for importing images

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
   }
 }
