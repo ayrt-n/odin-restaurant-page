04fd61f99cf7ff41e067646a4574ec2eb06a333a
ayrton <ayrton.parkinson1@gmail.com>
2022-08-15 08:37:56 -0400

    Add menuDiv which dynamically builds menu HTML

--- src/menuTab.js
+++ src/menuTab.js
@@ -1,7 +1,52 @@
-// Create main section div
-const mainDiv = document.createElement('div');
-mainDiv.classList.add('main-section');
+import pizzaPreview from './pizza-preview.png';
 
-// Create content for menu tab
+// Create menu div and content for menu tab
 const menuDiv = document.createElement('div');
-menuDiv.classList
+menuDiv.classList.add('menu-container', 'card');
+
+const menuPizzaHeader = document.createElement('h1');
+menuPizzaHeader.innerHTML = 'Pizza!!';
+menuDiv.appendChild(menuPizzaHeader);
+
+// Iterate through the list of menu items and append
+const menuItemsContainer = document.createElement('div');
+menuItemsContainer.classList.add('menu-items-container');
+
+const menuItemList = {
+  Cheese: '$16.99',
+  Pepperoni: '$16.99',
+  Supreme: '$18.99',
+  Canadian: '$18.99',
+  'Meat Lovers': '$21.99',
+  Veggie: '$18.99',
+  Mystery: '$24.99'
+}
+
+for (const item in menuItemList) {
+  const menuItemDiv = document.createElement('div');
+  menuItemDiv.classList.add('menu-item')
+
+  const menuItem = document.createElement('span');
+  menuItem.classList.add('menu-name');
+  menuItem.innerHTML = item
+
+  const menuPrice = document.createElement('span');
+  menuPrice.classList.add('menu-price');
+  menuPrice.innerHTML = menuItemList[item];
+
+  menuItemDiv.appendChild(menuItem);
+  menuItemDiv.appendChild(menuPrice);
+
+  menuItemsContainer.appendChild(menuItemDiv);
+}
+
+menuDiv.appendChild(menuItemsContainer);
+
+const pizzaImage = document.createElement('img');
+pizzaImage.src = pizzaPreview;
+pizzaImage.classList.add('menu-preview');
+
+menuDiv.appendChild(pizzaImage);
+
+
+export default menuDiv;
