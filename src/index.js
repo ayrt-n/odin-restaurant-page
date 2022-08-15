1ec85c837e265b2844764810be6a966d956f0eb5
ayrton <ayrton.parkinson1@gmail.com>
2022-08-15 07:47:04 -0400

    Add tabController and switchTab method to dynamically clear and load HTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import pageLoad from './pageLoad';
+import homeDiv from './homeTab';
+import contactDiv from './contactTab';
 
-window.addEventListener('DOMContentLoaded', pageLoad);
+pageLoad();
+
+// Module for switching between and clearing out website tabs
+const tabController = (() => {
+  const mainSection = document.getElementsByClassName('main-section')[0];
+  const tabs = document.querySelectorAll('.tab');
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', (e) => switchTab(e));
+  });
+
+  const tabContent = {
+    home: homeDiv,
+    menu: 'menuDiv',
+    contact: contactDiv
+  };
+
+  let currentTab = 'home'
+
+  const switchTab = (e) => {
+    if (e.target.id === currentTab) {
+      console.log('Already loaded');
+    } else {
+      mainSection.textContent = '';
+      currentTab = e.target.id;
+      mainSection.appendChild(tabContent[currentTab]);
+    }
+  };
+})();
